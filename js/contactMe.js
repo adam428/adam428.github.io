aeb30b293170c9249a1dbea6996fd3291926e109
adam428 <61628292+adam428@users.noreply.github.com>
2020-07-09 16:29:58 +0200

    NEW_website

--- js/contactMe.js
+++ js/contactMe.js
@@ -1,8 +1,8 @@
 // Get the modal
-var modal = document.getElementById("myModal2");
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn2");
+var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
